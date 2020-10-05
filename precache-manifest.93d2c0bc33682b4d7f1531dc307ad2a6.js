bfb95b8ae77a69a8cac6b877d8590a4c35da55c9
Sir Robert Burbridge <sirrobert@gmail.com>
2020-10-04 22:48:41 -0400

    Updates

---
+++ precache-manifest.93d2c0bc33682b4d7f1531dc307ad2a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e571d2f7d53ae43f2e9300246941e39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73db8d13b021bbe7e438",
+    "url": "/static/css/2.72d22ee7.chunk.css"
+  },
+  {
+    "revision": "e22587e9ca88099d66d7",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "73db8d13b021bbe7e438",
+    "url": "/static/js/2.1bafa334.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1bafa334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22587e9ca88099d66d7",
+    "url": "/static/js/main.3be1292c.chunk.js"
+  },
+  {
+    "revision": "585078a1a4eb511ab9a7",
+    "url": "/static/js/runtime-main.c353c00b.js"
+  },
+  {
+    "revision": "ee9338301a0f37ae0c572b034427a9d4",
+    "url": "/static/media/thefarce-net-code.ee933830.png"
+  }
+]);
